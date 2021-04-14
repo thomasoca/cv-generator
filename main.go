0593ac493b59f2cc869f26299cc6c0e086e41d2d
Thomas Aquino Ariasoca <thomas.ariasoca@gmail.com>
2021-04-14 18:37:28 +0700

    add cors setting

--- main.go
+++ main.go
@@ -9,26 +9,43 @@ import (
 	"path/filepath"
 )
 
+func setupResponse(w *http.ResponseWriter, r *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func serveFile(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
 	decoder := json.NewDecoder(r.Body)
 	var user User
 	err := decoder.Decode(&user)
 	if err != nil {
+		w.Header().Set("Content-type", "application/json")
+		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"message": "Bad request"}`))
+		return
 	}
 	fname, err := createFile(user)
 	if err != nil {
+		w.Header().Set("Content-type", "application/json")
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(`{"message": "Internal server error"}`))
+		return
 	}
 
 	// Open file
 	f, err := os.Open(fname)
 	if err != nil {
+		w.Header().Set("Content-type", "application/json")
 		log.Println(err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message": "Failed processing file"}`))
 		return
 	}
 	defer f.Close()
@@ -38,7 +55,9 @@ func serveFile(w http.ResponseWriter, r *http.Request) {
 	// Stream to response
 	if _, err := io.Copy(w, f); err != nil {
 		log.Println(err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message": "Failed send file"}`))
+		return
 	}
 
 	e := os.RemoveAll(filepath.Dir(fname))
