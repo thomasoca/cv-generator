79e9aa637460fa2fdd4b66d2a0054d280f6b85d5
Thomas Aquino Ariasoca <thomas.ariasoca@gmail.com>
2021-04-17 10:00:39 +0700

    fix json parsing from input

--- image_handler.go
+++ image_handler.go
@@ -5,8 +5,7 @@ import (
 	"errors"
 	"image"
 	"image/jpeg"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/png"
 	"io"
 	"net/http"
 	"os"
@@ -42,25 +41,42 @@ func imageFromUrl(URL, dirName string) (string, error) {
 	return fileName, err
 }
 
-func imageFromBase64(data string, dirName string) (string, error) {
-
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(data))
-
-	m, _, err := image.Decode(reader)
-	if err != nil {
-		return "", err
-	}
+func imageWriter(imageData image.Image, dirName string, extension string) (string, error) {
 
 	//Encode from image format to writer
-	imageName := filepath.Join(dirName, "image.jpg")
+	imageName := filepath.Join(dirName, "image."+extension)
 	f, err := os.OpenFile(imageName, os.O_WRONLY|os.O_CREATE, 0777)
 	if err != nil {
 		return "", err
 	}
 
-	err = jpeg.Encode(f, m, &jpeg.Options{Quality: 75})
+	err = jpeg.Encode(f, imageData, &jpeg.Options{Quality: 75})
 	if err != nil {
 		return "", err
 	}
 	return imageName, err
+}
+
+func imageFromBase64(data string, dirName string) (string, error) {
+	coI := strings.Index(string(data), ",")
+	rawImage := string(data)[coI+1:]
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(rawImage))
+
+	switch strings.TrimSuffix(data[5:coI], ";base64") {
+	case "image/png":
+		pngImageData, err := png.Decode(reader)
+		if err != nil {
+			return "", err
+		}
+		imageName, err := imageWriter(pngImageData, dirName, "png")
+		return imageName, err
+	case "image/jpeg":
+		jpegImageData, err := jpeg.Decode(reader)
+		if err != nil {
+			return "", err
+		}
+		imageName, err := imageWriter(jpegImageData, dirName, "jpeg")
+		return imageName, err
+	}
+	return "", errors.New("unrecognized format")
 }
