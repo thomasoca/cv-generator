685e2a01dfcbed1243c03ebafe743515d7b70ab1
thomasoca <thomas.ariasoca@gmail.com>
2023-07-17 20:09:15 +0700

    refactor: remove failback download in installer

--- internal/installer/installer.go
+++ internal/installer/installer.go
@@ -74,12 +74,7 @@ func InstallPrerequisite() error {
 			tinyTexURL := "https://yihui.org/tinytex/install-bin-unix.sh"
 			if err := downloadFile(tinyTexURL, "install-bin-unix.sh"); err != nil {
 				fmt.Println("Failed to download install-bin-unix.sh:", err)
-				cmdArgs := []string{"-qO-", tinyTexURL}
-				if err := utils.RunCommand("wget", nil, nil, cmdArgs...); err != nil {
-					fmt.Println("Failed to download TinyTex:", err)
-					return err
-				}
-
+        return err
 			}
 			fmt.Println("install-bin-unix.sh downloaded successfully.")
 
